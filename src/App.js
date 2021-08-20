324282b8e5bf9e8537460561587c8a935eb1a65c
markmerchant415 <markmerchant415@gmail.com>
2021-08-20 17:14:06 +0530

    Header Completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+//import Particles from './Components/Particles';
 
 function App() {
   return (
+    <>
+    
     <Navbar/>
+    <Header/>
+    </>
   );
 }
 
